72f28364ee24387b99da5c53f89c855d3ef5bf67
posokhin-dmitry <79544402+Posokhin-Dmitry@users.noreply.github.com>
2021-12-27 18:50:38 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import s from './App.module.css';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+import useLocalStorage from './hooks/useLocalStorage';
+
+const prevContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', prevContacts);
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = data => {
+    const newContact = { ...data, id: uuidv4() };
+
+    if (
+      contacts
+        .map(contact => contact.name.toLowerCase())
+        .includes(data.name.toLowerCase())
+    ) {
+      alert(`Contact "${data.name}" already exists`);
+    } else {
+      setContacts(prevState => [newContact, ...prevState]);
+    }
+  };
+
+  const handleFilter = event => {
+    const { value } = event.currentTarget;
+    setFilter(value);
+  };
+
+  const getMatchingContacts = () => {
+    const optimizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(optimizedFilter),
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId),
+    );
+  };
+
+  const filteredContacts = getMatchingContacts();
+
+  return (
+    <div className={s.container}>
+      <section title="Phonebook" className={s.section}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={handleAddContact} />
+      </section>
+
+      <section title="Contacts" className={s.section}>
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={handleFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import s from './ContactForm.module.css';
+
+function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    onSubmit({ name, number });
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={s.form}>
+      <label className={s.formLabel}>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          onChange={handleChange}
+          className={s.formInput}
+        />
+      </label>
+
+      <label className={s.formLabel}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+          required
+          onChange={handleChange}
+          className={s.formInput}
+        />
+      </label>
+
+      <button type="submit" className={s.button}>
+        Add contact
+      </button>
+    </form>
+  );
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export default ContactForm;
